cdb0195d21efcc8f0fe35eb66d76dd1a387b061f
deepak <deepakkushwahsk@gmail.com>
2024-03-27 22:26:02 +0530

    operator

---
+++ operator.rb
@@ -0,0 +1,98 @@
+# Unary Operator
+# Unary operators expect a single operand to run on.
+
+# puts("Unary operator")   
+# puts(~5)   
+# puts(~-5)   
+# puts(!true)   
+# puts(!false) 
+# ----------------------------------------------------------------------------------------------------------------------------------------
+
+# Airthmetic Operator
+# Airthmetic operators take numerical values as operands and return them in a single value.
+# puts("add operator")   
+# puts(10 + 20)      
+# puts("subtract operator")   
+# puts(35 - 15)    
+# puts("multiply operator")   
+# puts(4 * 8)   
+# puts("division operator")   
+# puts(25 / 5)   
+# puts("exponential operator")   
+# puts(5 ** 2)   
+# puts("modulo operator")   
+# puts(25 % 4)  
+
+# puts "4".to_i + 6   // to_i convert string to intiger
+# puts "3" + "4" concate to string
+# ----------------------------------------------------------------------------------------------------------------------------------------
+# Bitwise Operator
+# Bitwise operators work on bits operands.
+# -----------------------------------------------------------------------------------------------------------------------------------------
+# Logical Operator
+# Logical operators work on bits operands.
+
+# Operator	Description
+# &&	AND operator
+# ||	OR operator
+# -----------------------------------------------------------------------------------------------------------------------------------------------
+
+# Ternary Operator
+# Ternary operators first check whether given conditions are true or false, then execute the condition.
+# puts("Ternary operator")   
+#  puts(2<5 ? 5:2)   
+#  puts(5<2 ? 5:2)   
+#-------------------------------------------------------------------------------------------------------------------------------------------------
+
+# Assignment Operator
+# Assignment operator assign a value to the operands.
+# Operator	Description
+# =	Simple assignment operator
+# +=	Add assignment operator
+a = 0
+a += 1
+puts a
+# -=	subtract assignment operator
+b = 0
+b -= 1
+puts b
+# *=	Multiply assignment operator
+c = 2
+c *= 3
+puts c
+# /=	Divide assignment operator
+# %=	Modulus assignment operator
+# **=	Exponential assignment operator
+# -----------------------------------------------------------------------------------------------------------------------------------------------
+# Comparison Operator
+# Comparison operators compare two operands.
+# Operator	Description
+# ==	Equal operator
+# !=	Not equal operator
+# >	left operand is greater than right operand
+# <
+# Right operand is greater than left operand
+# >=	Left operand is greater than or equal to right operand
+# <=	Right operand is greater than or equal to left operand
+# <=>	Combined comparison operator
+# .eql?	Checks for equality and type of the operands
+# equal?	Checks for the object ID
+# puts("Comparison operator")   
+# puts(2 == 5)   
+# puts(2 != 5)   
+# puts(2 > 5)   
+# puts(2 < 5)   
+# puts(2 >= 5)   
+# puts(2 <= 5)   
+#-------------------------------------------------------------------------------------------------------------------------------------------------
+# Range Operator
+# Range operators create a range of successive values consisting of a start, end and range of values in between.
+
+# The (..) creates a range including the last term and (...) creates a range excluding the last term.
+# For example, for the range of 1..5, output will range from 1 to 5.
+
+# and for the range of 1...5, output will range from 1 to 4.
+
+# Operator	Description
+# ..	Range is inclusive of the last term
+# ...	Range is exclusive of the last term
